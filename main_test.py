0005ce109d3b8ab10d4e1cf28fd9f8393ae3b5c9
Aidan Abbott <aabbott1216@gmail.com>
2021-04-10 12:55:11 -0400

    New

--- main_test.py
+++ main_test.py
@@ -3,7 +3,6 @@ import numpy as np
 import os
 from time import time
 from windowcapture import WindowCapture
-from vision import Vision
 
 # Change the working directory to the folder this script is in.
 # Doing this because I'll be putting the files from each video in their own folder on GitHub
@@ -12,14 +11,6 @@ os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
 # initialize the WindowCapture class
 wincap = WindowCapture('Overwatch')
-# initialize the Vision class
-vision_limestone = Vision('Capture.jpg')
-
-'''
-# https://www.crazygames.com/game/guns-and-bottle
-wincap = WindowCapture()
-vision_gunsnbottle = Vision('gunsnbottle.jpg')
-'''
 
 loop_time = time()
 while(True):
@@ -27,9 +18,7 @@ while(True):
     # get an updated image of the game
     screenshot = wincap.get_screenshot()
 
-    # display the processed image
-    points = vision_limestone.find(screenshot, 0.5, 'rectangles')
-    #points = vision_gunsnbottle.find(screenshot, 0.7, 'points')
+    cv.imshow('Computer Vision', screenshot)
 
     # debug the loop rate
     print('FPS {}'.format(1 / (time() - loop_time)))
